a1ce5f39bec35c3e8ec0641090f96df6e5c145c6
pranjalikawale <pranjalikawale1578@gmail.com>
2020-04-13 08:46:51 -1200

    ADDED: Select valid cell during turn

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -10,6 +10,11 @@ COMPUTER="Computer"
 declare -A board
 declare -A player=([$HUMAN]=O [$COMPUTER]=O)
 playing=" "
+counter=1
+row=-1
+col=-1
+isvalid=1
+
 # initialize board
 function board()
 {
@@ -84,7 +89,76 @@ function playFirst()
    echo "$playing(${player[$playing]}) win toss & play first"
 }
 
+# read inut for board
+function input()
+{
+   row=-1
+   col=-1
+   while true
+   do
+      if [ $HUMAN == $playing ]
+      then
+         read -p "Enter the row from 1-3" row
+         read -p "Enter the column from 1-3" col
+         ((row--))
+         ((col--))
+      else
+			row="$(getInput)"
+   		col="$(getInput)"
+      fi
+      valid
+      if [[ (($isvalid -eq 1)) ]]
+      then
+         insert $playing
+         break
+      fi
+   done
+}
 
+# get random input for row and col for computer
+function getInput()
+{
+  echo $((RANDOM%3))
+}
+
+# check for valid row and col
+function valid()
+{
+   isEmpty
+   if [[ $isvalid -eq 1 ]]
+   then
+      if [[ (($row -lt 0)) && (($row -gt $ROW)) && (($col -lt 0)) && (($col -gt $COLUMN)) ]]
+      then
+         if [ $HUMAN == $playing ]
+         then
+            echo "Invalid $row & $col value !!!"
+         fi
+         isvalid=0
+      fi
+   fi
+}
+
+# check for empty space in board
+function isEmpty()
+{
+   if [[ ${board[$row,$col]} == " " ]]
+   then
+      isvalid=1
+   else
+      if [ $HUMAN == $playing ]
+      then
+         echo "Already taken !!!"
+      fi
+      isvalid=0
+   fi
+}
+
+# insert into the board
+function insert()
+{
+   board[$row,$col]="${player[$playing]}"
+   ((counter++))
+}
 
 # ticTacToc main function
 function ticTacToe()
@@ -94,6 +168,8 @@ function ticTacToe()
 	assignSymbol
 	displaySymbol
 	playFirst
+	input
+	display
 }
 
 # invoke ticTacToe
