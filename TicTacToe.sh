89aefde472bfe631c9a8b6a187394787cba4b48a
pranjalikawale <pranjalikawale1578@gmail.com>
2020-04-13 08:14:14 -1200

    ADDED: Assign symbol to player

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -3,9 +3,12 @@
 # constant
 ROW=3
 COLUMN=3
+HUMAN="Human"
+COMPUTER="Computer"
 
 # variable
 declare -A board
+declare -A player=([$HUMAN]=O [$COMPUTER]=O)
 
 # initialize board
 function board()
@@ -41,11 +44,41 @@ function display()
    done
 }
 
+# assign symbol to player
+function assignSymbol()
+{
+   if [[ "$(getRandom)" -eq 0 ]]
+   then
+      player[$HUMAN]=X
+   else
+      player[$COMPUTER]=X
+   fi
+}
+
+# return random nos
+function getRandom()
+{
+   MAX=1
+   MIN=0
+   echo $((RANDOM%($MAX-$MIN+1)+$MIN))
+}
+
+# display symbol
+function displaySymbol()
+{
+   for symbol in "${!player[@]}"
+   do
+      echo "$symbol received ${player[$symbol]} symbol"
+   done
+}
+
 # ticTacToc main function
 function ticTacToe()
 {
 	board
 	display
+	assignSymbol
+	displaySymbol
 }
 
 # invoke ticTacToe
